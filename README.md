fd5c2997ae6b7b3cb58564d8e303daa1c8c5f6b4
ILYA UTS <ilya.uts@jpmorgan.com>
2016-12-05 15:10:52 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+![Alt text](TwP.png?raw=true)
+
+# TwitterPeruser
+Peruse Twitter, maybe discover something!
+
+# Instructions
+Twitter Peruser relies on a couple of external libraries and databases:
+* TwitterAPI
+* Redis
+
+To install:
+
+1) Install TwitterAPI:
+
+    pip install TwitterAPI
+
+2) Install Redis:
+
+    wget http://download.redis.io/redis-stable.tar.gz
+    tar xvzf redis-stable.tar.gz
+    cd redis-stable
+    make
+
+3) Start the Redis server:
+
+    redis-server
+
+4) In a new terminal tab, test Redis (should expect a PONG response):
+
+    redis-cli ping
+
+5) Install wordcloud:
+
+    pip install wordcloud
+
+6) Copy keys.template and call it keys.py. Replace bracketed values in that file with your secret Twitter keys
+
+7) Run application:
+
+    python peruse.py [query in quotes] [number of tweets to be used]
---
+++ peruse.py
@@ -0,0 +1,163 @@
+# Headers
+from threading import Thread
+import urllib
+import json
+import redis
+from os import path
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+from TwitterAPI import TwitterAPI, TwitterRestPager, TwitterRequestError, TwitterConnectionError
+from keys import consumer_key, consumer_secret, access_token_key, access_token_secret
+
+# Instantiate the database
+r_server = redis.Redis("localhost")
+
+# Print all the values for a key
+def printValues(server, key):
+    print server.lrange(key, 0, -1)
+
+# One time results
+def singleCall(query='blockchain', count = 50):
+    r = api.request('search/tweets', {'q':query, 'count':count })
+    for item in r.get_iterator():
+        if 'text' in item:
+            print item['text']
+    return
+
+# Paging the results
+def pagingCall(server, query='blockchain', key='Tweets', count = 200, limit = 200):
+    # Keep tabs on the number of entries
+    numEntries = 1
+
+    r = TwitterRestPager(api, 'search/tweets', {'q':query, 'count':count})
+    for item in r.get_iterator():
+        if 'text' in item:
+            # Pass-in tweets into a list
+            server.rpush(key, item['text'])
+
+            if(numEntries % 100 == 0):
+                print 'Entries so far', numEntries
+                #printValues(server, key)
+            if(numEntries == limit):
+                break
+
+            # Move on to the next entry
+            numEntries += 1
+
+        elif 'message' in item and item['code'] == 88:
+            print 'SUSPEND, RATE LIMIT EXCEEDED: %s\n' % item['message']
+            break
+
+    return
+
+def iterateTweets(sentiment, server, pager):
+    count = 0
+    for tweet in pager.get_iterator():
+        count += 1
+        if 'text' in tweet:
+            server.rpush(sentiment, tweet['text'])
+
+            # Print progress periodically
+            if(count % 100 == 0):
+                print sentiment, count
+
+def joinAndEncode(wordList):
+    return urllib.quote_plus(''.join(wordList))
+
+# Paging sentiment comparison
+def sentimentComparison(server, query1 = 'Hillary Clinton', query2 = 'Donald Trump', count = 50):
+    # Keep tabs on the number of entries
+    numEntries = 1
+
+    positiveSentiment = ' :)'
+    negativeSentiment = ' :('
+
+    h_plus = TwitterRestPager(api, 'search/tweets', {'q':joinAndEncode([query1, positiveSentiment]), 'count':count})
+    h_minus = TwitterRestPager(api, 'search/tweets', {'q':joinAndEncode([query1, negativeSentiment]), 'count':count})
+    d_plus = TwitterRestPager(api, 'search/tweets', {'q':joinAndEncode([query2, positiveSentiment]), 'count':count})
+    d_minus = TwitterRestPager(api, 'search/tweets', {'q':joinAndEncode([query2, negativeSentiment]), 'count':count})
+
+    tweets_list = [h_plus, h_minus, d_plus, d_minus]
+    tweets_titles = ['h+', 'h-', 'd+', 'd-']
+    tweets_lengths = []
+
+    t1 = Thread(target=iterateTweets, args=(tweets_titles[0], r_server, h_plus))
+    t2 = Thread(target=iterateTweets, args=(tweets_titles[1], r_server, h_minus))
+    t3 = Thread(target=iterateTweets, args=(tweets_titles[2], r_server, d_plus))
+    t4 = Thread(target=iterateTweets, args=(tweets_titles[3], r_server, d_minus))
+
+    t1.start()
+    t2.start()
+    t3.start()
+    t4.start()
+
+    print 'sentimentComparison() complete!'
+
+    return
+
+def generateWordCloud(text):
+    # read the mask / color image
+    # taken from http://jirkavinse.deviantart.com/art/quot-Real-Life-quot-Alice-282261010
+    d = path.dirname(__file__)
+
+    cloud_coloring = np.array(Image.open(path.join(d, "us-mask-white.png")))
+    stopwords = set(STOPWORDS)
+    stopwords.add("said")
+
+    wc = WordCloud(background_color="black", max_words=2000, mask=cloud_coloring,
+                   stopwords=stopwords, max_font_size=40, random_state=42)
+    # generate word cloud
+    wc.generate(text)
+
+    # create coloring from image
+    image_colors = ImageColorGenerator(cloud_coloring)
+
+    # show
+    plt.imshow(wc)
+    plt.axis("off")
+    plt.show()
+
+def getTweetsList(server, key):
+    # Returns all tweets
+    return server.lrange(key, 0, -1)
+
+def getVal(server, key):
+    return server.get(key)
+
+def clearList(server, key):
+    server.ltrim(key, -1, 0)
+    return
+
+def deleteKey(server, key):
+    server.delete(key)
+    return
+
+api = TwitterAPI(consumer_key, consumer_secret, access_token_key, access_token_secret, 'oAuth2')
+
+# Make a single call
+query = 'Donald Trump'
+key = 'Tweets'
+numTweets = 50
+limit = 250
+
+# Take user input into account
+if(len(sys.argv) > 1):
+    query = sys.argv[1]
+
+# if(len(sys.argv) > 2):
+#     limit = int(sys.argv[2])
+
+# Start by deleting the old key
+deleteKey(r_server, key)
+
+# Move on to getting tweets
+pagingCall(r_server, query, key, numTweets, limit)
+tweets_text = getTweetsList(r_server, key)
+
+generateWordCloud(''.join(tweets_text))
+
+# sentimentComparison(r_server)
